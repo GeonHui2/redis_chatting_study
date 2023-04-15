eb7eb292f208d038e2f77e6bd1b6de9374d596c4
이건희 <leegeonhui@igeonhuiui-MacBookAir.local>
2023-04-15 19:01:28 +0900

    redis_chatting_study

---
+++ src/test/java/chat/chatting_study/ChattingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package chat.chatting_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
